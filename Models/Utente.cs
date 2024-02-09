9527261725927b321d42c7824940f85f1a77b5b8
Luca Bernardi <101919348+Fujcyuj@users.noreply.github.com>
2024-02-09 16:00:58 +0100

    Add files via upload

---
+++ Models/Utente.cs
@@ -0,0 +1,9 @@
+namespace Esercizio.Models;
+
+public class Utente
+{
+    public string? Nome { get; set; }
+    public string? Cognome { get; set; }
+    public string? Tipo { get; set; }
+    public string? Email { get; set; }
+}
